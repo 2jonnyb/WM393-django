2817800088fb16e15af21356027cc4cfc13edc42
Jonathan Ball <2jonnyb@gmail.com>
2022-01-27 21:38:35 +0000

    edit models and create example users

--- WMGTSS/QnA/admin.py
+++ WMGTSS/QnA/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Question, Profile, Course, Student, Tutor, Answer
+
+admin.site.register(Question)
+admin.site.register(Profile)
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Tutor)
+admin.site.register(Answer)
--- WMGTSS/QnA/models.py
+++ WMGTSS/QnA/models.py
@@ -1,32 +1,34 @@
+from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     univeristy_id = models.CharField(max_length=20, blank=True)
+    first_name = models.CharField(max_length=99, blank=True)
+    surname = models.CharField(max_length=99, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True) 
+    courses = models.ManyToManyField(Course, blank=True)
     
-class Course(models.Model):
-    name = models.CharField(max_length=100)
-
-class Person(models.Model):
-    courses = models.ManyToManyField(Course)
-
-class Student(Person):
+class Student(Profile):
     pass
 
-class Tutor(Person):
+class Tutor(Profile):
     pass
 
 class Question(models.Model):
-    submitted_by = models.ForeignKey(Student)
+    submitted_by = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True)
     title = models.CharField(max_length=99)
     body = models.TextField(max_length=999)
+    likes = models.IntegerField()
 
 
-class Answer(models.Mode):
-    question = models.OneToOneField(Question)
-    answered_by = models.ForeignKey(Tutor)
+class Answer(models.Model):
+    question = models.OneToOneField(Question, on_delete=models.CASCADE)
+    answered_by = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True)
     body = models.TextField(max_length=999)
