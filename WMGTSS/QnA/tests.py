25dc4b3be87c4a37f084fd61f9c6dd111c18ff13
Jonathan Ball <2jonnyb@gmail.com>
2022-02-07 02:17:38 +0000

    added test case

--- WMGTSS/QnA/tests.py
+++ WMGTSS/QnA/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from .models import Course, Profile, Tutor, Student, Board, Question, Answer, Like
 # Create your tests here.
+
+class BasicTest(TestCase):
+    def test_course_model(self):
+        course = Course()
+        course.name = "WM393"
+        course.save()
+
+        record = Course.objects.get(pk=1)
+        self.assertEqual(record, course)
